3498ee58506993eb26e7279682394a829b0ac3a2
IFPB <rodrigobentor2014@gmail.com>
2017-10-10 09:56:43 -0300

    Projeto iniciado

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,26 +2,24 @@ angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope) {})
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+// Controlador para listar postos
+.controller('PostoCtrl', function($scope, Chats) {
 
   $scope.chats = Chats.all();
   $scope.remove = function(chat) {
     Chats.remove(chat);
   };
+
 })
 
+// Buscar por ID
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
   $scope.chat = Chats.get($stateParams.chatId);
 })
 
-.controller('AccountCtrl', function($scope) {
+
+// Controlador cadastro
+.controller('CadastroCtrl', function($scope) {
   $scope.settings = {
     enableFriends: true
   };
